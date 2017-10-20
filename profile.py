7adad695b5387f7d0a73e7f16ad74424ef1ecd2a
Jonathan Ellithorpe <jde@cs.stanford.edu>
2017-10-20 20:06:07 +0000

    Added hardware type parameter.

--- profile.py
+++ profile.py
@@ -21,6 +21,13 @@ num_nodes = range(1,1000)
 pc.defineParameter("num_nodes", "Number of Hadoop Nodes", 
     portal.ParameterType.INTEGER, 1, num_nodes)
 
+hardware_types = [ ("m510", "m510 (CloudLab Utah, Intel Xeon-D)"),
+                   ("m400", "m400 (CloudLab Utah, 64-bit ARM)") ]
+
+pc.defineParameter("hardware_type", "Hardware Type",
+                   portal.ParameterType.NODETYPE, 
+                   hardware_types[0], hardware_types)
+
 params = pc.bindParameters()
 
 node_names = ["master"]
@@ -34,6 +41,7 @@ clan.vlan_tagging = True
 
 for name in node_names:
   node = request.RawPC(name)
+  node.hardware_type = params.hardware_type
   
   # Install and execute a script that is contained in the repository.
   node.addService(pg.Execute(shell="sh", command="/local/repository/setup.sh"))
