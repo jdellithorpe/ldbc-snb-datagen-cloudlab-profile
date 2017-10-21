57fae17dba8f50b38a9f4b3451bd76dfe6e56733
Jonathan Ellithorpe <jde@cs.stanford.edu>
2017-10-21 21:20:04 +0000

    Added Java installation to setup.sh
    Added Java OpenJDK to install script.

--- setup.sh
+++ setup.sh
@@ -6,3 +6,10 @@ apt-get --assume-yes install mosh vim tmux pdsh tree axel
 
 # NFS
 apt-get --assume-yes install nfs-kernel-server nfs-common
+
+# Java
+apt-get install openjdk-6-jdk
+export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-amd64
+
+# For git commit messages
+export EDITOR=vim
