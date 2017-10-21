ce85b44a85fd5e8d741616445e4255a2162af9a4
Jonathan Ellithorpe <jde@cs.stanford.edu>
2017-10-21 21:49:11 +0000

    Forgot to put --assume-yes on apt-get install.
    For this script to run without user input, --assume-yes needs to be
    added to the apt-get command.

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ apt-get --assume-yes install mosh vim tmux pdsh tree axel
 apt-get --assume-yes install nfs-kernel-server nfs-common
 
 # Java
-apt-get install openjdk-6-jdk
+apt-get --assume-yes install openjdk-6-jdk
 export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-amd64
 
 # For git commit messages
