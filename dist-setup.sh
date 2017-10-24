c3b987a5fab4d16a5a79052d21bdd84e8b077644
Jonathan Ellithorpe <jde@cs.stanford.edu>
2017-10-24 19:29:36 +0000

    Changed dist-setup to use root account.

--- dist-setup.sh
+++ dist-setup.sh
@@ -5,7 +5,8 @@ do
   if [ "$ip" != "127.0.0.1" ]
   then
 		echo "Running setup @ $ip..."
-    ssh -o StrictHostKeyChecking=no -n $USER@$ip "/local/repository/setup.sh" &
+    USERNAME=$USER
+    sudo ssh -o StrictHostKeyChecking=no -n root@$ip "sudo su $USERNAME; cd ~; /local/repository/setup.sh" &
   fi
 done < /etc/hosts
 
