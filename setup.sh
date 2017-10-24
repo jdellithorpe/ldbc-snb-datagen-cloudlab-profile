dd0134ef482dc330155301f9bc26ac7246949b85
Jonathan Ellithorpe <jde@cs.stanford.edu>
2017-10-24 18:37:54 +0000

    Added moving root ssh keys to USERNAME.
    Added code to copy over root's ssh keys to the given USERNAME to allow
    USERNAME to ssh into other machines without a password.

--- setup.sh
+++ setup.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
+USERNAME="jde"
+HOME="/users/$USERNAME"
 
 # Install common utilities
-sudo apt-get update
-sudo apt-get --assume-yes install mosh vim tmux pdsh tree axel
+apt-get update
+apt-get --assume-yes install mosh vim tmux pdsh tree axel
 
 # NFS
-sudo apt-get --assume-yes install nfs-kernel-server nfs-common
+apt-get --assume-yes install nfs-kernel-server nfs-common
 
 # Java
-sudo apt-get --assume-yes install openjdk-6-jdk maven
+apt-get --assume-yes install openjdk-6-jdk maven
 
 # Modify bashrc
-cat > ~/.bashrc <<EOM
+cat > $HOME/.bashrc <<EOM
 export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-amd64
 export EDITOR=vim
 EOM
 
 # Modify ssh config
-cat > ~/.ssh/config <<EOM
+cat > $HOME/.ssh/config <<EOM
 Host *
     StrictHostKeyChecking no
 EOM
 
-env > ~/info.txt
+while read -r ip linkin linkout hostname
+do 
+  if [ "$ip" != "127.0.0.1" ]
+  then
+    ssh -o StrictHostKeyChecking=no -n root@$ip "cat .ssh/authorized_keys >> $HOME/.ssh/authorized_keys; cp .ssh/id_rsa $HOME/.ssh/id_rsa; chown $USERNAME:ramcloud-PG0 $HOME/.ssh/id_rsa"
+  fi
+done < /etc/hosts
 
 # Download Hadoop
-#wget http://archive.apache.org/dist/hadoop/core/hadoop-2.6.0/hadoop-2.6.0.tar.gz
-#tar -xvzf hadoop-2.6.0.tar.gz
-
-ls -alh /users > ~/usersdir.txt
+cd $HOME
+wget http://archive.apache.org/dist/hadoop/core/hadoop-2.6.0/hadoop-2.6.0.tar.gz
+chown $USERNAME:ramcloud-PG0 hadoop-2.6.0.tar.gz
+tar -xvzf hadoop-2.6.0.tar.gz
