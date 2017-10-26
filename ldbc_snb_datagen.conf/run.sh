f237aee7d1edc53598288277b1130e303eef7cf6
jde <jde@master.jde-qv29897.ramcloud-pg0.utah.cloudlab.us>
2017-10-26 12:10:54 -0600

    Added hadoop and ldbc configuration files.
    These are the intial configuration files for hadoop and ldbc_snb_datagen.
    
    One thing to note here is that certain files like hadoop.conf/hdfs-site.xml and hadoop.conf/slaves contain data that is dependent on the exact cluster setup. These need to be written more dynamically based on information gleaned about the cluster, either through observing the cluster setup after instantiation, or somehow modifying things during cluster setup and configuration.

---
+++ ldbc_snb_datagen.conf/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+DEFAULT_HADOOP_HOME=/opt/hadoop-2.6.0 #change to your hadoop folder
+DEFAULT_LDBC_SNB_DATAGEN_HOME=/opt/ldbc_snb_datagen #change to your ldbc_socialnet_dbgen folder
+
+# allow overriding configuration from outside via environment variables
+# i.e. you can do
+#     HADOOP_HOME=/foo/bar LDBC_SNB_DATAGEN_HOME=/baz/quux ./run.sh
+# instead of changing the contents of this file
+HADOOP_HOME=${HADOOP_HOME:-$DEFAULT_HADOOP_HOME}
+LDBC_SNB_DATAGEN_HOME=${LDBC_SNB_DATAGEN_HOME:-$DEFAULT_LDBC_SNB_DATAGEN_HOME}
+
+export HADOOP_HOME
+export LDBC_SNB_DATAGEN_HOME
+
+mvn clean
+mvn -DskipTests assembly:assembly 
+
+$HADOOP_HOME/bin/hadoop jar $LDBC_SNB_DATAGEN_HOME/target/ldbc_snb_datagen-0.2.7-jar-with-dependencies.jar $LDBC_SNB_DATAGEN_HOME/params.ini
+
+rm -f m*personFactors*
+rm -f .m*personFactors*
+rm -f m*activityFactors*
+rm -f .m*activityFactors*
+rm -f m0friendList*
+rm -f .m0friendList*
