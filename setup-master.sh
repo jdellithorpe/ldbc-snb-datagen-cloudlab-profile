f2d46805cd89fa6eb0add24e9be7be17dc050274
Jonathan Ellithorpe <jde@cs.stanford.edu>
2017-10-31 00:00:22 +0000

    Updated setup-master to put files in new loc.
    The LDBC dataset generation files now need to be placed in the mounted
    dataset in order to download the dataset to the correct location.

--- setup-master.sh
+++ setup-master.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/ldbc/ldbc_snb_datagen.git /opt/ldbc_snb_datagen
-chmod -R g=u /opt/ldbc_snb_datagen
-cp /local/repository/ldbc_snb_datagen.conf/* /opt/ldbc_snb_datagen/
+git clone https://github.com/ldbc/ldbc_snb_datagen.git /mnt/dataset/ldbc_snb_datagen
+chmod -R g=u /mnt/dataset/ldbc_snb_datagen
+cp /local/repository/ldbc_snb_datagen.conf/* /mnt/dataset/ldbc_snb_datagen/
